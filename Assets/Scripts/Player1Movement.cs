c2c2f5fbab32e5deaf9cab9d695d224462a121e4
Andreas von Holy <vhland002@myuct.ac.za>
2015-03-28 22:41:51 +0200

    some intital work done on 2 players in game

---
+++ Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Movement : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(20,20);
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("W")) {
+			//we need to move up
+		}
+		
+		if (Input.GetButtonDown("S")) {
+			//we need to move down
+		}
+
+		if (Input.GetButtonDown("D")) {
+			//we need to move right
+		}
+
+		if (Input.GetButtonDown("A")) {
+			//we need to move left
+		}
+
+
+		
+
+	}
+}
---
+++ Assets/Scripts/Player2Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("UPARROW")){
+			//we need to move up
+		}
+		
+		if (Input.GetButtonDown("DOWNARROW")) {
+			//we need to move down
+		}
+		
+		if (Input.GetButtonDown("RIGHTARROW")) {
+			//we need to move right
+		}
+		
+		if (Input.GetButtonDown("LEFTARROW")){
+			//we need to move left
+		}
+	}
+}
