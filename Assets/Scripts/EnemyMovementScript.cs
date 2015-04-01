329b203c3bab3e89c6e7f1fdb1151371c01ddff7
Andreas von Holy <vhland002@myuct.ac.za>
2015-04-01 23:15:44 +0200

    enemy AI done

--- Assets/Scripts/EnemyMovementScript.cs
+++ Assets/Scripts/EnemyMovementScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class EnemyMovementScript : MonoBehaviour {
 
-	public Vector2 speed = new Vector2 (10, 10);
+	public Vector2 speed = new Vector2 (3, 3);
 	
 	public Vector2 direction = new Vector2 (0,0);
 
@@ -12,14 +12,16 @@ public class EnemyMovementScript : MonoBehaviour {
 	public bool chasing = false;
 
 
-	public float detectionRange = 50f;
-	public float chasingRange = 70f;
-	
+	public float detectionRange = 30f;
+	public float chasingRange = 6.5f;
+
 	private GameObject player1;
 
 	private GameObject player2;
 
 	private GameObject prey;
+
+
 
 
 
@@ -29,60 +31,66 @@ public class EnemyMovementScript : MonoBehaviour {
 
 		var rayCast = Physics2D.Raycast(this.transform.position, direction, detectionRange, 1 << LayerMask.NameToLayer("Player"));
 		Debug.DrawRay(transform.position, new Vector3(direction.x,direction.y,0), Color.red);
-		var gap = Vector3.Distance(this.transform.position, prey.transform.position);
+		if (chasing){
 
-		if (chasing && gap < chasingRange){
-			print("chasing");
-			Vector3 target = prey.transform.position;
-			Vector3 hunter = this.transform.position;
+			float gap = Vector3.Distance(this.transform.position, prey.transform.position);
+			if (gap < chasingRange){
+				//print("chasing " + gap);
+				Vector3 target = prey.transform.position;
+				Vector3 hunter = this.transform.position;
 
-			//check x
-			if (hunter.x < target.x ){
-				//move right
-				direction.x = 1;
-				//print("hunter right");
-			}
-			else if (hunter.x > target.x){
-				//move left
-				direction.x  = -1;
-				//print("hunter left");
-				
+				//check x
+				if (hunter.x < target.x ){
+					//move right
+					direction.x = 1;
+					//print("hunter right");
+				}
+				else if (hunter.x > target.x){
+					//move left
+					direction.x  = -1;
+					//print("hunter left");
+					
+				}
+				else{
+					//stay still
+					direction.x = 0;	
+					//print("hunter stay x");
+					
+				}
+
+				//check y
+				if (hunter.y < target.y ){
+					//move up
+					direction.y = 1;
+					//print("hunter up");
+					
+				}
+				else if (hunter.y > target.y){
+					//move down
+					direction.y = -1;
+					//print("hunter down");
+					
+				}
+				else{
+					//stay still
+					direction.y = 0;
+					///print("hunter stay y");
+					
+				}
 			}
 			else{
-				//stay still
-				direction.x = 0;	
-				//print("hunter stay x");
-				
-			}
-
-			//check y
-			if (hunter.y < target.y ){
-				//move up
-				direction.y = 1;
-				//print("hunter up");
-				
-			}
-			else if (hunter.y > target.y){
-				//move down
-				direction.y = -1;
-				//print("hunter down");
-				
-			}
-			else{
-				//stay still
-				direction.y = 0;
-				//print("hunter stay y");
-				
+				//print("lost");
+				chasing = false;
 			}
 		}
 		else if(rayCast){
 			prey = rayCast.transform.gameObject;
 			chasing = true;
-			print("raycasting");
+			//print("raycasting");
 		}
 		else{
 			//if not in range
-			print("chilling");
+			//print("chilling");
 
 			counter++;
 			chasing = false;
@@ -116,13 +124,13 @@ public class EnemyMovementScript : MonoBehaviour {
 				counter = 0;
 			}
 			// a new vector to devcide our 
-			Vector3 movement = new Vector3 (speed.x * direction.x, speed.y * direction.y, 0);
-			
-			movement *= Time.deltaTime;
-			
-			transform.Translate (movement);
-		}
 
+		}
+		Vector3 movement = new Vector3 (speed.x * direction.x, speed.y * direction.y, 0);
+		
+		movement *= Time.deltaTime;
+		
+		transform.Translate (movement);
 		//need to check distances
 		//if true
 			//check which player is closer
