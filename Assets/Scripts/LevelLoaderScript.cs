88854b480f04d7e3289f126977501d2671a25ef8
Andreas von Holy <vhland002@myuct.ac.za>
2015-04-03 21:39:20 +0200

    gameover and game won screens added, old class deleted

--- Assets/Scripts/LevelLoaderScript.cs
+++ Assets/Scripts/LevelLoaderScript.cs
@@ -14,13 +14,10 @@ public class LevelLoaderScript : MonoBehaviour {
 
 	void Update(){
 		if ( (player1 && player2) || (player1 && player2Dead) || (player1Dead && player2)){
-			//loadNextLevel ("Level2");
 			Application.LoadLevel("GameWon");
 		}
 		if (player1Dead && player2Dead){
 			//print("should be loading next level");
-			player1Dead = true;
-			player2Dead = true;
 			Application.LoadLevel("GameOver");
 		}
 
